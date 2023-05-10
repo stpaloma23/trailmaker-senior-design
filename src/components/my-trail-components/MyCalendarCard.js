5e04b66bf8e2e2bab1701c72f4a2d6ad3043f6df
Paloma St.Clair <stpaloma23@gmail.com>
2023-05-10 00:31:25 -0400

    calendar added

--- src/components/my-trail-components/MyCalendarCard.js
+++ src/components/my-trail-components/MyCalendarCard.js
@@ -1,7 +1,16 @@
+import Calendar from "react-calendar";
+import React, { useState } from 'react';
+// import 'react-calendar/dist/Calendar.css';
+
 function MyCalendarCard() {
+    const [value, onChange] = useState(new Date());
+
     return (
         <div className="my-trail-card">
             <h1>My Calendar</h1>
+            <div className="calendar-card-container">
+                <Calendar locale='en-US' weekStartsOn={0} onChange={onChange} value={value}/>
+            </div>
         </div>
     )
 }
